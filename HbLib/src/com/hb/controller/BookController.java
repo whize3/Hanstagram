51b04995f7a5f3c4d0050fae427920a58c17143f
my <my@110.12.28.146>
2016-07-13 23:47:28 +0900

    a

--- HbLib/src/com/hb/controller/BookController.java
+++ HbLib/src/com/hb/controller/BookController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/MyBookController")
+@WebServlet("/BookController")
 public class BookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
