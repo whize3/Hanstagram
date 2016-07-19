6d3b3f2563605570eec0c3e4f8761cbadba805a0
hb1024 <hb1024@hb1024-PC>
2016-07-19 16:47:20 +0900

    aaa

---
+++ HbLib/src/com/hb/mybatis/StudyroomDAO.java
@@ -0,0 +1,43 @@
+package com.hb.mybatis;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class StudyroomDAO {
+	private static SqlSession ss;
+	
+	private synchronized static SqlSession getSql(){
+		if(ss == null){
+			ss = DBService.getFactory().openSession();
+		}
+		return ss;
+	}
+	
+	public List<StudyroomVO> allStudyRoom(){
+		List<StudyroomVO> list = getSql().selectList("getRoom");
+		return list;
+	}
+	
+	public List<StudyroomVO> getTime(String date){
+		System.out.println("getTime");
+		List<StudyroomVO> list = getSql().selectList("getTime",date);
+		System.out.println("list.size="+list.size());
+		return list;
+	}
+	
+	public void roomReserve(Studyroom_ReserveVO rvo){
+		System.out.println("roomReserve"+rvo.getS_num());
+		try{
+			getSql().insert("reserve", rvo);
+			getSql().commit();
+		}catch(Exception e){
+			e.printStackTrace();
+			getSql().rollback();
+			
+		}
+	}
+}
