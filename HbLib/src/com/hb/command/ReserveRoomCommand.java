70005ec67ee0596113714ae78b18e630d6547da2
hb1024 <hb1024@hb1024-PC>
2016-07-21 16:01:48 +0900

    ssss

--- HbLib/src/com/hb/command/ReserveRoomCommand.java
+++ HbLib/src/com/hb/command/ReserveRoomCommand.java
@@ -9,14 +9,13 @@ import com.hb.mybatis.Studyroom_ReserveVO;
 public class ReserveRoomCommand implements Command{
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("reserveRoomCommand is started"+request.getParameter("roomNum"));
 		Studyroom_ReserveVO rvo = new Studyroom_ReserveVO();
 		rvo.setId(request.getParameter("id"));
 		rvo.setS_num(request.getParameter("roomNum"));
 		rvo.setSr_date(request.getParameter("rDate"));
 		rvo.setStart_time(request.getParameter("start"));
 		rvo.setEnd_time(request.getParameter("end"));
-		rvo.setSr_state("reserve");
+		rvo.setSr_state("1");
 		
 		StudyroomDAO sdao = new StudyroomDAO();
 		sdao.roomReserve(rvo);
