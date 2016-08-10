15cd611aa1bd7ebeee09ca1fda2813965b3e9782
hb1024 <hb1024@hb1024-PC>
2016-08-10 15:50:36 +0900

    suhyun

--- Hansta/src/spring/project/db/Dao.java
+++ Hansta/src/spring/project/db/Dao.java
@@ -18,22 +18,22 @@ public class Dao {
 		this.template = template;
 	}
 	
-	// 메인에서검색
+	// 硫붿씤�뿉�꽌寃��깋
 	public List<UsersVO> search(String keyword){
 		return template.selectList("search", keyword);		
 	}
 	
-	// 팔로우 리스트 불러오기
+	// �뙏濡쒖슦 由ъ뒪�듃 遺덈윭�삤湲�
 	public List<FollowVO> getFollowList(String id){
 		return template.selectList("follow", id);
 	}
 	
-	// 아이디 주고 게시글 불러오기
+	// �븘�씠�뵒 二쇨퀬 寃뚯떆湲� 遺덈윭�삤湲�
 	public List<BoardVO> getBooardList(String id){
 		return template.selectList("board", id);
 	}
 	
-	// 게시글 좋아요 상태 확인하기
+	// 寃뚯떆湲� 醫뗭븘�슂 �긽�깭 �솗�씤�븯湲�
 	public LikeVO likeState(String b_idx, String id){
 		Map<String, String> map = new HashMap<>();
 		map.put("b_idx", b_idx);
@@ -42,7 +42,7 @@ public class Dao {
 		return lvo;
 	}
 	
-	// 좋아요 삽입
+	// 醫뗭븘�슂 �궫�엯
 	public void insertLike(String b_idx, String id){
 		Map<String, String> map = new HashMap<>();
 		map.put("b_idx", b_idx);
@@ -50,7 +50,7 @@ public class Dao {
 		template.insert("insertlike", map);
 	}
 	
-	// 좋아요 삭제
+	// 醫뗭븘�슂 �궘�젣
 	public void deleteLike(String b_idx, String id){
 		Map<String, String> map = new HashMap<>();
 		map.put("b_idx", b_idx);
@@ -78,6 +78,26 @@ public class Dao {
 		map.put("id", id);
 		map.put("c_content", c_content);
 		template.insert("insertcomment", map);
+	}
+	
+	public void followState(String id, String followeeId){
+		Map<String, String> map = new HashMap<>();
+		map.put("followeeId", followeeId);
+		map.put("id", id);
+		
+	}
+	public FollowVO followCheck(String id, String followeeId){
+		Map<String, String> map = new HashMap<>();
+		map.put("followeeId", followeeId);
+		map.put("id", id);
+		FollowVO result = template.selectOne("followCnt", map);
+		return result;
+	}
+	public void insertFollow(String id, String followeeId){
+		Map<String, String> map = new HashMap<>();
+		map.put("followeeId", followeeId);
+		map.put("id", id);
+		template.insert("follow", map);
 	}
 	
 }
